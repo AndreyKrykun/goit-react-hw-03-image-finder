5bc14356eb5c9736e34531038ac9d4969299d0ce
AndreyKrykun <andreykrykun1995@gmail.com>
2022-08-27 15:30:31 +0300

    finished-update

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,7 +2,7 @@ import { Component } from "react";
 import { createPortal } from "react-dom";
 import PropTypes from 'prop-types';
 
-const modalRoot = document.querySelector('#modal-root');
+const modalRoot = document.querySelector('#root');
 
 export default class Modal extends Component {
     componentDidMount() {
